59384c95e612554482e6ef097204ba849f79fbd5
Lauren Paik <laurpaik@Laurens-MacBook-Pro.local>
2017-01-04 12:21:12 -0500

    unit 10 assignment

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,7 +3,7 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
-if (cardOne === cardTwo) {
+/*if (cardOne === cardTwo) {
 	alert("You found a match!");
 }
 else {
@@ -14,4 +14,13 @@ if (cardThree === cardFour) {
 }
 else {
 	alert("Sorry, try again.");
+}*/
+var gameBoard = document.getElementById('game-board');
+var createCards = function(x){
+	for (var i=0; i < x; i++) {
+		var newCard = document.createElement("div");
+		newCard.className = "card";
+		gameBoard.appendChild(newCard);
+	}
 }
+createCards(4);
